bd9c3a43051a5a8c4cebd1e193dfa9d5fb9ab7e7
Changyong(Jack) <98437328+devTabasco@users.noreply.github.com>
2022-11-30 16:12:50 +0900

    Add files via upload

---
+++ test/src/main/webapp/resources/js/common.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+ //<input type="text" name="groupName" placeholder="GROUP NAME" />
+ function lengthCheck(obj) {
+	//서버로 전송할 데이터 길이의 유효성 판단
+	const data = [["groupName",2,20] , ["groupCeo",2,5] , ["groupPin",6,6]];
+	let result = false;
+	for(let i=0;i<data.length;i++){
+		if(obj.getAttribute("name") == data[i][0]){
+			result = (obj.value.length >= data[i][1] && 
+			obj.value.length <= data[i][2])?true:false;
+			
+			break;
+		}
+	}
+	return result;
+}
+
+function createForm(name, action, method){
+	const form = document.createElement("form");
+	if(name != "") form.setAttribute("name", name);
+	form.setAttribute("action", action);
+	form.setAttribute("method", method);
+	return form;
+}
